19e6fffde9a9a0305d02d01c6abf8362d1305c38
Pankaj <mukharjeepankaj@live.com>
2020-12-05 13:52:07 +0530

    Second Commit for GIT

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,18 +1,32 @@
 
 public class HelloWorld {
+	
+	static int j=5;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		HelloWorld object = new HelloWorld();
+		object.welcome();
 		
 		for(int i=1;i<=10;i++) {
 			
-			System.out.println("Welcome to Git");
+			int res=j*i;
+			System.out.println(j+"*"+i+"="+res);
 			
-			System.out.println("Hello World"+"["+i+"]");
-			
-			System.out.println("From Jenkins");
 		}
+		
+		object.tearDown();
 
+	}
+	
+	public void welcome() {
+		
+		System.out.println("Welcome to Git");
+	}
+	
+	public void tearDown() {
+		
+		System.out.println("Good Bye from Git");
 	}
 
 }
